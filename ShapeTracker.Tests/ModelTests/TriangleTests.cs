62724671fe39d5a680bcf9be61f21ba05459fe7f
onurkaymak <onurkaymak@outlook.com>
2023-09-18 14:57:41 -0700

    add passing test, code for ClearAll_DeletesAllTriangleInList

--- ShapeTracker.Tests/ModelTests/TriangleTests.cs
+++ ShapeTracker.Tests/ModelTests/TriangleTests.cs
@@ -120,5 +120,19 @@ namespace ShapeTracker.Tests
       // Assert
       CollectionAssert.AreEqual(expected, actualResult);
     }
+
+    [TestMethod]
+    public void ClearAll_DeletesAllTriangleInList_Void()
+    {
+      Triangle tri1 = new Triangle(2, 2, 9);
+      Triangle tri2 = new Triangle(21, 3, 9);
+      Triangle tri3 = new Triangle(1, 3, 9);
+      List<Triangle> expected = new List<Triangle> { };
+
+      Triangle.ClearAll();
+
+      CollectionAssert.AreEqual(expected, Triangle.GetAll());
+    }
+
   }
 }
